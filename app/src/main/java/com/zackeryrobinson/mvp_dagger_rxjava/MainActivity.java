2dc981c84ab4448945b56b5c47013c5f8a9aacd5
Zack Robinson <zwrprofessional@gmail.com>
2017-10-07 18:49:49 -0400

    Initial commit

---
+++ app/src/main/java/com/zackeryrobinson/mvp_dagger_rxjava/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zackeryrobinson.mvp_dagger_rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // I owe you this assignment.
+        // My next step is to review this https://caster.io/courses/mvp/ and this https://caster.io/courses/rxjava/
+        
+
+
+    }
+}
